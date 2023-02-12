15791e8e6e365a373425a77d9f4dac5277cb372f
Thierry de Pauw <thierry@thinkinglabs.io>
2023-02-12 10:37:57 +0100

    Fix F401 unused import

--- DownloadDMARCFilesFromGmail/GmailAuth.py
+++ DownloadDMARCFilesFromGmail/GmailAuth.py
@@ -1,7 +1,6 @@
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
 
 import os
 import pickle
